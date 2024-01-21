db7a5de91f25fe29ee9811c500f61a31ea646049
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-01-21 10:22:21 -0300

    Add scrollbar

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ import { ButtonStyle } from './ButtonStyle'
 export default function Button({id ,textButton}) {
   return(
     <ButtonStyle id={id}>
-      <a href="curriculo.pdf" target='_blank' download='Curriculo Bruno Mendonça'>{textButton}</a>
+      <a href="/curriculo.pdf" target='_blank' download='Curriculo Bruno Mendonça'>{textButton}</a>
     </ButtonStyle>
   )
 }
--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -20,10 +20,23 @@ html {
   @media (max-width: 750px) {
     font-size: 55%;
   }
+  
 }
 
 body{
   background-color: #1d1d1d;
   height: 100vh;
+  
+}
+body::-webkit-scrollbar {
+  width: 10px;
+}
+body::-webkit-scrollbar-track {
+  background-color: #1d1d1d;
+}
+body::-webkit-scrollbar-thumb {
+  background-color: #14abc6;
+  border-radius: 20px;
+  padding: 1rem;
 }
 `
