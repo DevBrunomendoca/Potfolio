0a898e7bbddca7b3516e003db1759cd64aae5a6f
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-01-22 12:42:57 -0300

    Update curriculum

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ import { ButtonStyle } from './ButtonStyle'
 export default function Button({id ,textButton}) {
   return(
     <ButtonStyle id={id}>
-      <a href="curriculo.pdf" target='_blank' download='curriculo'>{textButton}</a>
+      <a href="curriculo.pdf" target='_blank' download>{textButton}</a>
     </ButtonStyle>
   )
 }
